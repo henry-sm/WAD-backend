2a049a4f98ae0a9a8f2a7ee67366b42c4dd2c2ef
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-04-16 22:34:37 +0530

    Employee Controller Updated

--- src/main/java/com/wadproject/qrcode/Employee/EmployeeController.java
+++ src/main/java/com/wadproject/qrcode/Employee/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.wadproject.qrcode.Employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -13,25 +18,39 @@ public class EmployeeController {
 
     // Create a new Employee
     @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return null;
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     // Delete Employee by ID
     @DeleteMapping("/{id}")
-    public ResponseBody deleteEmployeeById(@PathVariable String id) {
-        return null;
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable String id) {
+        if (!employeeRepository.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        employeeRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
     //Mark Atendance of an Employee
     @PostMapping("/{id}/attend")
-    public ResponseBody markEmployeeAttendance(@PathVariable String id) {
+    public ResponseEntity<Map<String,String>> markEmployeeAttendance(@PathVariable String id) {
         Optional<Employee> _employee = employeeRepository.findById(id);
+        Map<String,String> map = new HashMap<>();
+
         if(_employee.isPresent()){
             Employee emp = _employee.get();
             emp.markAttendance();
+            employeeRepository.save(emp);
+        }else{
+            map.put("message","Employee not Found");
+            return  new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
         }
+
+        
+        map.put("message","Attendance Updated");
         //Implement the response
-        return null ;
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 }
