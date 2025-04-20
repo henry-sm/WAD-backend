c6120579b417ab1857ef567b3e08af6fb1fd6d19
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-04-20 23:40:35 +0530

    Deployed URL Cors

--- src/main/java/com/wadproject/qrcode/AppConfig.java
+++ src/main/java/com/wadproject/qrcode/AppConfig.java
@@ -24,7 +24,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://localhost:4200", "https://192.168.1.2:4200")
+                        .allowedOrigins("http://localhost:4200", "https://localhost:4200", "https://192.168.1.2:4200","https://angularproject-seven-jet.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
